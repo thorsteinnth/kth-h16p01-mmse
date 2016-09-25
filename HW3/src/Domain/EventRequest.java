8e4de166e6f5d09b7e11adb2ce5406a6d5a0d97a
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-25 23:47:22 +0200

    ll

--- HW3/src/Domain/EventRequest.java
+++ HW3/src/Domain/EventRequest.java
@@ -1,7 +1,18 @@
 package Domain;
 
+import java.util.Date;
+import java.math.BigDecimal;
+
 /**
  * Created by tts on 25/09/2016.
  */
-public class EventRequest {
+public class EventRequest
+{
+    public Client Client;
+    public String Description;
+    public Date StartDateTime;
+    public Date EndDateTime;
+    public int NumberOfAttendees;
+    public String PreferencesDescription;
+    public BigDecimal Budget;
 }
