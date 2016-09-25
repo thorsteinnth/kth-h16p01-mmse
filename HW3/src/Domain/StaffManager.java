f47b618290ee02136f9879fda7852a59bd37a343
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-25 22:12:31 +0200

    recruitment request

--- HW3/src/Domain/StaffManager.java
+++ HW3/src/Domain/StaffManager.java
@@ -1,7 +1,11 @@
 package Domain;
 
+import java.util.ArrayList;
+
 /**
  * Created by tts on 25/09/2016.
  */
-public class StaffManager extends User {
+public class StaffManager extends User
+{
+    public ArrayList<RecruitmentRequest> RecruitmentRequests;
 }
