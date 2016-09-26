843eae1e60bf1d3cfa6faee0e1bd5b16c049e0ae
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-09-26 12:49:15 +0200

    SEniorCustomerSErviceOFficer

--- HW3/src/Domain/SeniorCustomerServiceOfficer.java
+++ HW3/src/Domain/SeniorCustomerServiceOfficer.java
@@ -1,8 +1,31 @@
 package Domain;
 
+import java.util.ArrayList;
+
 /**
  * Created by tts on 25/09/2016.
  */
 public class SeniorCustomerServiceOfficer extends User
 {
+    public ArrayList<Client> CreatedClients;
+
+    public void ApproveOrRejectEventRequest(EventRequest request)
+    {}
+
+    public Client CreateClient()
+    {
+        return new Client();
+    }
+
+    public void SearchEventRequests()
+    {}
+
+    public void ViewEventRequest()
+    {}
+
+    public void SearchClientRecords()
+    {}
+
+    public void ViewClientRecord()
+    {}
 }
