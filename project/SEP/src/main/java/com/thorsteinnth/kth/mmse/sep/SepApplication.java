b15f17b6ed67a36c663f3f4696f3aea7e4583409
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-10-13 21:18:01 +0200

    adding SEP foldre

---
+++ project/SEP/src/main/java/com/thorsteinnth/kth/mmse/sep/SepApplication.java
@@ -0,0 +1,12 @@
+package com.thorsteinnth.kth.mmse.sep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepApplication.class, args);
+	}
+}
---
+++ project/SEP/src/test/java/com/thorsteinnth/kth/mmse/sep/SepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thorsteinnth.kth.mmse.sep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
