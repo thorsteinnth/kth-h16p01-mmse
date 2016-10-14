361c2acc877ad6baadadaef31299b37bea2587cb
Fannar Magnússon <fannar@kth.se>
2016-10-14 16:00:38 +0200

    browse client records

--- kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Service/ClientService.java
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Service/ClientService.java
@@ -4,9 +4,9 @@ import com.thorsteinnth.kth.mmse.sepcli.Domain.Client;
 
 public class ClientService
 {
-    public Client createClient(String name, String address, String email, String phoneNumber)
+    public Client createClient(String id, String name, String address, String email, String phoneNumber)
     {
         // TODO Get from repository
-        return new Client(name, address, email, phoneNumber);
+        return new Client(id, name, address, email, phoneNumber);
     }
 }
