83f8038d19b11431dbceaf013a9fbb4cc9291367
Fannar Magnússon <fannar@kth.se>
2016-10-14 15:09:30 +0200

    user management

--- kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/AppData.java
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/AppData.java
@@ -8,10 +8,12 @@ import java.util.ArrayList;
 public class AppData
 {
     public static User loggedInUser;
+    public static ArrayList<User> users;
     public static ArrayList<Client> clients;
 
     public AppData()
     {
+        users = new ArrayList<User>();
         clients = new ArrayList<Client>();
     }
 }
--- kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Service/UserService.java
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Service/UserService.java
@@ -2,6 +2,8 @@ package com.thorsteinnth.kth.mmse.sepcli.Service;
 
 import com.thorsteinnth.kth.mmse.sepcli.Domain.User;
 
+import java.util.ArrayList;
+
 public class UserService
 {
     public User login(String email, String password)
@@ -9,5 +11,20 @@ public class UserService
         // TODO Get from repository
 
         return new User(email, password, User.Role.GeneralUser);
+    }
+
+    public User createUser(String email, String password, User.Role role)
+    {
+        return new User(email, password, role);
+    }
+
+    public ArrayList<User> deleteUser(ArrayList<User> userList, User user)
+    {
+        if(userList.contains(user))
+        {
+            userList.remove(user);
+        }
+
+        return  userList;
     }
 }
