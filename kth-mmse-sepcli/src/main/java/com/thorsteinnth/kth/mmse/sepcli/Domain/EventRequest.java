96ce66e1b3fae88595e305eb54ea97ba92faabd6
Fannar Magnússon <fannar@kth.se>
2016-10-15 15:20:13 +0200

    event request

---
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Domain/EventRequest.java
@@ -0,0 +1,14 @@
+package com.thorsteinnth.kth.mmse.sepcli.Domain;
+
+// TODO Refactor, add abstract request class?
+public class EventRequest
+{
+    public Client client;
+    public String title;
+    public String description;
+
+    public EventRequest()
+    {
+
+    }
+}
