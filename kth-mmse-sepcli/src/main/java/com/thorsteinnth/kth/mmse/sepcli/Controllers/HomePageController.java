8e66154afe51272be824b9dfa221fd6bbf82e42a
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-10-16 18:25:51 +0200

    adding UIOperation, use that in homepagecontroller

--- kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Controllers/HomePageController.java
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Controllers/HomePageController.java
@@ -2,38 +2,78 @@ package com.thorsteinnth.kth.mmse.sepcli.Controllers;
 
 import com.thorsteinnth.kth.mmse.sepcli.AppData;
 import com.thorsteinnth.kth.mmse.sepcli.CliHelper;
+import com.thorsteinnth.kth.mmse.sepcli.Repository.RequestEnvelopeRepository;
+import com.thorsteinnth.kth.mmse.sepcli.Service.RequestMailService;
+import com.thorsteinnth.kth.mmse.sepcli.UIOperation;
 
 import java.util.ArrayList;
 
 public class HomePageController extends BaseController
 {
+    private RequestMailService requestMailService;
+
+    public HomePageController()
+    {
+        this.requestMailService = new RequestMailService(new RequestEnvelopeRepository());
+    }
+
     public void displayPage()
     {
         CliHelper.newLine();
         CliHelper.write("This is the homepage for user: " + AppData.loggedInUser.email);
-        CliHelper.write("Please select one of the following operations:");
-        CliHelper.write("1. Client management");
-        CliHelper.write("2. Create event request");
-        CliHelper.write("9. Logout");
-        CliHelper.write("0. Quit");
+
+        ArrayList<UIOperation> operations = buildUIOperationList();
         ArrayList<String> validInputs = new ArrayList<String>();
-        validInputs.add("1");
-        validInputs.add("2");
-        validInputs.add("9");
-        validInputs.add("0");
 
-        String input = CliHelper.getInput("Select an operation (0-9)", validInputs);
+        CliHelper.write("Please select one of the following operations:");
 
-        if (input.equals("1"))
-            clientManagement();
-        else if(input.equals("2"))
-            createEventRequest();
-        else if (input.equals("9"))
-            logout();
-        else if (input.equals("0"))
-            closeApplication();
+        for (UIOperation op : operations)
+        {
+            CliHelper.write(op.toDisplayString());
+            validInputs.add(Integer.toString(op.getNumber()));
+        }
+
+        String input = CliHelper.getInput("Select an operation", validInputs);
+
+        UIOperation selectedOperation = UIOperation.getUIOperationWithNumberFromList(
+                Integer.parseInt(input),
+                operations
+        );
+
+        if (selectedOperation == null)
+        {
+            CliHelper.write("ERROR: Unknown operation selected");
+            displayPage();
+        }
         else
-            System.out.println("ERROR: Unknown command");
+        {
+            selectedOperation.executeCommand();
+        }
+    }
+
+    private boolean userHasIncomingRequests()
+    {
+        if (!this.requestMailService.getRequestEnvelopesForUser(AppData.loggedInUser).isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    private ArrayList<UIOperation> buildUIOperationList()
+    {
+        ArrayList<UIOperation> operations = new ArrayList<>();
+
+        UIOperation.Command clientManagement = () -> clientManagement();
+        UIOperation.Command createEventRequest = () -> createEventRequest();
+        UIOperation.Command logout = () -> logout();
+        UIOperation.Command quit = () -> closeApplication();
+
+        operations.add(new UIOperation(1, "Client management", clientManagement));
+        operations.add(new UIOperation(2, "Create event request", createEventRequest));
+        operations.add(new UIOperation(3, "Logout", logout));
+        operations.add(new UIOperation(4, "Quit", quit));
+
+        return operations;
     }
 
     private void clientManagement()
