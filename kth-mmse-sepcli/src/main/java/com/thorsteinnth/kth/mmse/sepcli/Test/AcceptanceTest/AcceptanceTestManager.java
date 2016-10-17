2299a655f71f85def0cdbb8dffd40deb95b7b737
Fannar Magnússon <fannar@kth.se>
2016-10-17 17:35:31 +0200

    added AcceptanceTestManager

---
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Test/AcceptanceTest/AcceptanceTestManager.java
@@ -0,0 +1,34 @@
+package com.thorsteinnth.kth.mmse.sepcli.Test.AcceptanceTest;
+
+import java.io.InputStream;
+
+public class AcceptanceTestManager
+{
+    private static String testNumber;
+
+    //TODO figure out how to assert test result
+
+    public static void runAcceptanceTest(String testToRun)
+    {
+        testNumber = testToRun;
+
+        if (testNumber.equals("1"))
+        {
+            EventRequestAcptTest.runTest();
+        }
+        else
+        {
+            //Run another acceptance test
+        }
+    }
+
+    public static InputStream getInputStream()
+    {
+        if (testNumber.equals("1"))
+            return EventRequestAcptTest.getInputStream();
+        else
+            return EventRequestAcptTest.getInputStream();
+
+        //else return another input stream
+    }
+}
