59258e6b680ef40da0fca437df0ebc1bacc63441
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-10-17 16:41:47 +0200

    access control in homepagecontroller

--- kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Domain/AccessFunction.java
+++ kth-mmse-sepcli/src/main/java/com/thorsteinnth/kth/mmse/sepcli/Domain/AccessFunction.java
@@ -2,6 +2,8 @@ package com.thorsteinnth.kth.mmse.sepcli.Domain;
 
 public enum AccessFunction
 {
+    clientManagement,   // Everyone that can do anything with clients has this right
+    requestManagement,  // Everyone that can do anything with requests has this right
     createClientRecord,
     browseClientRecords,
     createEventRequest,
